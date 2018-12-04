be3c918742daae766955a017772b457d0ad1c05c
killi8n <killi8n@killi8nui-MacBookPro.local>
2018-12-04 19:08:33 +0900

    4:50

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,12 +1,28 @@
-const nicolas = {
-  name: "Nicolas",
-  age: 18,
-  gender: "male"
-};
+const people = [
+  {
+    id: 1,
+    name: "Nicolas",
+    age: 18,
+    gender: "male"
+  },
+  {
+    id: 2,
+    name: "Nicolas2",
+    age: 28,
+    gender: "male"
+  },
+  {
+    id: 3,
+    name: "Nicola3",
+    age: 38,
+    gender: "male"
+  }
+];
 
 const resolvers = {
   Query: {
-    person: () => nicolas
+    people: () => people,
+    person: id => people.filter(person => person.id)
   }
 };
 
